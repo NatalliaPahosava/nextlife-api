0ba4827748c4a2ec4a5b706ab44b5fe82a7ef9bb
NatalliaPahosava <mulato4ka114@gmail.com>
2022-10-12 09:44:48 -0400

    added notes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import { MongoClient, ObjectId } from 'mongodb'
 import 'dotenv/config'
 
+///---connections---///
 const URI='mongodb+srv://NatalliaPahosava:sladk11mo1@nataliapogosova.1b8tti3.mongodb.net/test'
 const PORT=4040
 const client= new MongoClient(URI)
@@ -15,22 +16,24 @@ app.use(cors())
 app.use(express.json())
 
 app.listen(PORT,()=>console.log('API is running on PORT 4040'))
+
+//--------Get(Read)---------//
 app.get('/',async(req,res)=>{
 const allItems=await items.find().toArray()
 res.send(allItems)})
-//POST
 
+//--------POST(Create)-------//
 app.post('/',async(req,res)=>{
 await items.insertOne(req.body)  
 res.send('Item was added')
 })
-//DELETE
+//---------DELETE(Delete)-----//
 app.delete('/', async(req,res)=>{
   let id=new ObjectId(req.query._id)
   await items.findOneAndDelete({_id: id})
   res.json('Item was deleted')
 })
-//---------PUT---------//
+//---------PUT(Update)--------//
 app.put('/',async (req,res)=>{
    await items.findOneAndUpdate(req.query,{ $set:req.body })
     res.json('Item was updated')
